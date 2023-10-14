75fbde2466b5c72414f8fa8a028fd98a37a064f9
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2023-10-14 20:57:18 +0200

    Initial commit

--- app/src/main/java/com/example/cuadrante_de_compose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrante_de_compose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.cuadrante_de_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cuadrante_de_compose.ui.theme.Cuadrante_de_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Cuadrante()
                 }
             }
         }
@@ -30,17 +43,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Cuadrante(){
+    Column (
+        Modifier.fillMaxWidth()
+    ){
+        Row(
+            Modifier.weight(1f)
+        ) {
+            Cuadro(tile = "Text composable",
+                information = "Displays text and follows the recommended Material Design guidelines.",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f))
+            Cuadro(tile = "Image composable",
+                information = "Creates a composable that lays out and draws a given Painter class object.",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f))
+        }
+        Row (
+            Modifier.weight(1f)
+        ) {
+            Cuadro(tile = "Row composable",
+                information = "A layout composable that places its children in a horizontal sequence",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f))
+            Cuadro(tile = "Column composable",
+                information = "A layout composable that places its children in a vertical sequence.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f))
+        }
+    }
+
+
 }
+
+@Composable
+fun Cuadro(tile: String, information: String, backgroundColor: Color, modifier: Modifier){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = tile,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = information,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Cuadrante_de_ComposeTheme {
-        Greeting("Android")
+        Cuadrante()
     }
 }
