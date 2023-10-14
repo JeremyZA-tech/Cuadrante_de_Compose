f513142411dbcd66034e21c97ae9fd4f8ada4981
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2023-10-14 20:57:18 +0200

    Initial commit

--- app/src/main/java/com/example/cuadrante_de_compose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrante_de_compose/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -44,9 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Cuadrante(){
-    Column (
-        Modifier.fillMaxWidth()
-    ){
+    Column {
         Row(
             Modifier.weight(1f)
         ) {
